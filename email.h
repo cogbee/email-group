dbc1d3f5b1d3c0d3a5c72f62b24fa414aa93f40c
cogbee <cogbee@163.com>
2014-05-07 08:26:51 +0800

    Create email.h

---
+++ email.h
@@ -0,0 +1,18 @@
+/*****************************
+author:jaffer
+time:2013-03-7
+version:1.0
+******************************/
+
+#ifndef _EMAIL_H
+#define _EMAIL_H
+
+#include <iostream>
+#include <string.h>
+
+extern CString yourEmail ;
+extern CString yourPasswd ;
+extern CString suffixOfEmail;//邮箱后缀
+int run();//程序运行入口
+
+#endif
